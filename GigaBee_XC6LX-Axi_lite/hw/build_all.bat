6dea1fb5fd746f5a03ec193d7c6b25f4fad8f927
Oleksandr Kiyenko <a.kienko@gmail.com>
2012-06-21 21:56:21 +0300

    Updated ready_for_download

--- GigaBee_XC6LX-Axi_lite/hw/build_all.bat
+++ GigaBee_XC6LX-Axi_lite/hw/build_all.bat
@@ -2,31 +2,43 @@ rem Configure environment
 call C:\Xilinx\13.2\ISE_DS\settings64.bat
 
 @rem Prepare lx45 project
-@rem @copy /y system.xmp.lx45 system.xmp
-@rem @copy /y system_incl.make.lx45 system_incl.make
+@copy /y system.xmp.lx45 system.xmp
+@copy /y system_incl.make.lx45 system_incl.make
 @rem @copy /y data\system.ucf.lx45 data\system.ucf
 @rem Clean project
-@rem make -f system.make hwclean
+make -f system.make hwclean
 @rem Generate bitstream
-@rem make -f system.make init_bram
+make -f system.make bits
 @rem Copy result
-copy /y implementation\system.bit system_lx45.bit
-copy /y implementation\system_bd.bmm system_bd_lx45.bmm
-copy /y implementation\download.bit download_lx45.bit
+copy /y implementation\system.bit ..\ready_for_download\system_lx45.bit
+copy /y implementation\system_bd.bmm ..\ready_for_download\system_bd_lx45.bmm
 @rem Clean project
-@rem make -f system.make hwclean
+make -f system.make hwclean
 
+@rem Prepare lx150 project
+@copy /y system.xmp.lx150 system.xmp
+@copy /y system_incl.make.lx150 system_incl.make
+@rem @copy /y data\system.ucf.lx150 data\system.ucf
+@rem Clean project
+make -f system.make hwclean
+@rem Generate bitstream
+make -f system.make bits
+@rem Copy result
+@copy /y implementation\system.bit ..\ready_for_download\system_lx150.bit
+@copy /y implementation\system_bd.bmm ..\ready_for_download\system_bd_lx150.bmm
+@rem Clean project
+make -f system.make hwclean
 
-@copy download_lx45.bit fpga.bit
-@impact -batch etc\bit2mcs.cmd
-@move fpga.mcs TE0600-LX45.mcs
-@rem Remove logs
-@del fpga.bin fpga.prm fpga.cfi fpga.bit fpga.sig
-
-
-@rem Remove files
-@del usb.bin 
-@del Bootload.ini
-@del _impactbatch.log
-
-pause
+@rem Prepare lx100 project
+@copy /y system.xmp.lx100 system.xmp
+@copy /y system_incl.make.lx100 system_incl.make
+@rem @copy /y data\system.ucf.lx100 data\system.ucf
+@rem Clean project
+make -f system.make hwclean
+@rem Generate bitstream
+make -f system.make bits
+@rem Copy result
+@copy /y implementation\system.bit ..\ready_for_download\system_lx100.bit
+@copy /y implementation\system_bd.bmm ..\ready_for_download\system_bd_lx100.bmm
+@rem Clean project
+make -f system.make hwclean
